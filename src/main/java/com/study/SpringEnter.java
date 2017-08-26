261acbb389a52357e41a7270c46678a5196bb2cd
Bqtangy <bqtangz@163.com>
2017-08-26 09:08:55 +0800

    learn spring frawkwork

--- src/main/java/com/study/SpringEnter.java
+++ src/main/java/com/study/SpringEnter.java
@@ -13,10 +13,13 @@ public class SpringEnter {
 
     public static void main(String[] args) {
 
-
         ApplicationContext  applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
 
         User user = (User) applicationContext.getBean("user");
+
+
+
+
         System.out.println("获取用户的姓名："+user.getUserName());
     }
 }
